06bc8674b484fc0cb4a7bb14b6fc56ff5df7b477
kimihito <tatsurotamashiro@gmail.com>
2015-04-11 23:55:44 +0900

    Googleカレンダーにアクセスするファイルを追加

---
+++ app/lib/calendar.rb
@@ -0,0 +1,24 @@
+require 'google/api_client'
+
+class Calendar
+  def initialize
+    @client = Google::APIClient.new
+    @client.authorization.client_id = ENV['CLIENT_ID']
+    @client.authorization.client_secret = ENV['CLIENT_SECRET']
+    @client.authorization.refresh_token = ENV['REFRESH_TOKEN']
+    @client.authorization.fetch_access_token!
+  end
+
+  def create_channel
+    service = @client.discovered_api('calendar', 'v3')
+    @client.execute!(
+      api_method: service.events.watch,
+      parameters: { calendarId: ENV['CALENDAR_ID'] },
+      body_object: {
+        id: SecureRandom.uuid(),
+        type: 'web_hook',
+        token: @client.authorization.access_token,
+        address: 'https://calm-eyrie-6230.herokuapp.com/notifications'
+      })
+  end
+end
