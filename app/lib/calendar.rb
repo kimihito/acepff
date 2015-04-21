22ba32713aec02dfaec7d8ea95f4c0d0ae47cc55
kimihito <tatsurotamashiro@gmail.com>
2015-04-21 16:46:13 +0900

    カレンダーのPush notificationを取得とイベントの内容を取得する

--- app/lib/calendar.rb
+++ app/lib/calendar.rb
@@ -7,17 +7,27 @@ class Calendar
     @client.authorization.client_secret = ENV['CLIENT_SECRET']
     @client.authorization.refresh_token = ENV['REFRESH_TOKEN']
     @client.authorization.fetch_access_token!
+    @service = @client.discovered_api('calendar', 'v3')
   end
 
   def create_channel
-    service = @client.discovered_api('calendar', 'v3')
     @client.execute!(
-      api_method: service.events.watch,
+      api_method: @service.events.watch,
       parameters: { calendarId: ENV['CALENDAR_ID'] },
       body_object: {
         id: SecureRandom.uuid(),
         type: 'web_hook',
         address: ENV['RECEIVE_URL']
       })
+  end
+
+  def get_events
+    @client.execute!(
+      api_method: @service.events.list,
+      parameters: {
+        calendarId: ENV['CALENDAR_ID'],
+        updatedMin: 1.minutes.ago.to_datetime.rfc3339
+      }
+    )
   end
 end
